dd5bdf74cf019a595941c1c35cc50ebea26a2844
jatin chauhan <81281135+jatin-98@users.noreply.github.com>
2022-12-22 04:11:26 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,14 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
-$config = [
-    'userClass' => \App\Models\User::class,
-    'db' => [
-        'dsn' => $_ENV['DB_DSN'],
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASSWORD'],
-    ],
-];
+$config = include '../config/db.php';
 
 $app = new App\Core\Application(dirname(__DIR__), $config);
 
